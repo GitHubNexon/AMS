64cca97f5279f71f525720045d3ad12d5e178c4c
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-05-02 14:54:43 +0800

    added update and get all for employee assets assignments

--- server/controller/employeeAssetsController.js
+++ server/controller/employeeAssetsController.js
@@ -40,8 +40,86 @@ const createEmployeeAssetsRecord = async (req, res) => {
   }
 };
 
+const updateEmployeeAssetsRecord = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedData = req.body;
 
+    const updatedEmployeeAssets = await employeeAssetsModel.findByIdAndUpdate(
+      id,
+      updatedData,
+      { new: true }
+    );
+
+    if (!updatedEmployeeAssets) {
+      return res.status(404).json({ message: "Record not found" });
+    }
+
+    res.status(200).json(updatedEmployeeAssets);
+  } catch (error) {
+    console.error(
+      "Error updating employee assets record:",
+      error.message,
+      error.stack
+    );
+    res.status(500).json({ message: "Error processing request" });
+  }
+};
+
+const getEmployeeAssetsRecord = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const keyword = req.query.keyword || "";
+    const sortBy = req.query.sortBy || "createdAt";
+    const sortOrder = req.query.sortOrder === "asc" ? -1 : 1;
+    const status = req.query.status;
+
+    const query = {
+      ...(keyword && {
+        $or: [
+          { parNo: { $regex: keyword, $options: "i" } },
+          { employeeName: { $regex: keyword, $options: "i" } },
+        ],
+      }),
+      ...(status &&
+        status === "isDeleted" && {
+          "Status.isDeleted": true,
+        }),
+      ...(status &&
+        status === "isArchived" && {
+          "Status.isArchived": true,
+        }),
+    };
+
+    const sortCriteria = {
+      "Status.isDeleted": 1,
+      "Status.isArchived": 1,
+      [sortBy]: sortOrder,
+    };
+
+    const totalItems = await employeeAssetsModel.countDocuments(query);
+
+    const employeeAssets = await employeeAssetsModel
+      .find(query)
+      .sort(sortCriteria)
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.json({
+      totalItems,
+      totalPages: Math.ceil(totalItems / limit),
+      currentPage: page,
+      employeeAssets: employeeAssets,
+    });
+  } catch (error) {
+    console.error("Error getting employee assets record:", error.message);
+    res.status(500).json({ message: "Error processing request" });
+  }
+};
 
 module.exports = {
   createEmployeeAssetsRecord,
+  updateEmployeeAssetsRecord,
+  getEmployeeAssetsRecord,
 };
--- server/routes/employeeAssetsRoutes.js
+++ server/routes/employeeAssetsRoutes.js
@@ -8,4 +8,13 @@ router.use(authenticateToken);
 // Create a new employee assets record
 router.post("/create", EmployeeAssetsController.createEmployeeAssetsRecord);
 
+// Update an existing employee assets record
+router.patch(
+  "/update/:id",
+  EmployeeAssetsController.updateEmployeeAssetsRecord
+);
+
+// Get all employee assets records
+router.get("/get-all", EmployeeAssetsController.getEmployeeAssetsRecord);
+
 module.exports = router;
