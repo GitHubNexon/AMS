64ac53d4a3d291592dd78b4f5951da90e9480264
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-05-06 08:33:47 +0800

    added the tabs for Asset Management

---
+++ client/src/Navigation/AssetsNav.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import AssetsTable from "../Sub-pages/AssetsTable";
+
+const TABS = [
+  "Overview",
+  "Asset Records",
+  "Employee Records",
+  "Issuance",
+  "Turn In",
+  "History",
+  "Reports",
+];
+
+const AssetsNav = () => {
+  const [activeTab, setActiveTab] = useState("Overview");
+
+  return (
+    <div className="w-full">
+      <ul className="flex border-b text-[0.9em] pt-4 mb-2 overflow-auto">
+        {TABS.map((tab) => (
+          <li
+            key={tab}
+            className={`py-1 px-4 border-b-[5px] whitespace-nowrap cursor-pointer ${
+              activeTab === tab ? "border-green-500" : "border-transparent"
+            }`}
+            onClick={() => setActiveTab(tab)}
+          >
+            {tab}
+          </li>
+        ))}
+      </ul>
+
+      <div className="p-4">
+        {activeTab === "Overview" && <div>Overview Content</div>}
+        {activeTab === "Asset Records" && (
+          <div>
+            <AssetsTable />{" "}
+          </div>
+        )}
+        {activeTab === "Employee Records" && (
+          <div>Employee Records Content</div>
+        )}
+        {activeTab === "Issuance" && <div>Issuance Content</div>}
+        {activeTab === "Turn In" && <div>Turn In Content</div>}
+        {activeTab === "History" && <div>History Content</div>}
+        {activeTab === "Reports" && <div>Reports Content</div>}
+      </div>
+    </div>
+  );
+};
+
+export default AssetsNav;
--- client/src/Pages/AssetManagement.jsx
+++ client/src/Pages/AssetManagement.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import AssetsTable from "../Sub-pages/AssetsTable";
 import EmployeeAssetsTable from "../Sub-pages/EmployeeAssetsTable";
+import AssetsNav from "../Navigation/AssetsNav";
 
 const AssetManagement = () => {
   return (
     <div>
-      <AssetsTable />
-      <EmployeeAssetsTable />
+      {/* <AssetsTable />
+      <EmployeeAssetsTable /> */}
+      <AssetsNav />
     </div>
   );
 };
