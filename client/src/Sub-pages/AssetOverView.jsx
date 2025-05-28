2c7401685456d8132a6e972febb1245a156bd445
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-05-28 15:06:41 +0800

    remove testing

--- client/src/Sub-pages/AssetOverView.jsx
+++ client/src/Sub-pages/AssetOverView.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import AssetsEmployeePicker from "../Components/AssetsEmployeePicker"; // Adjust the import path
 
 const AssetOverView = () => {
   return (
     <div>
       <h2>Asset Overview</h2>
-      <AssetsEmployeePicker />
     </div>
   );
 };
