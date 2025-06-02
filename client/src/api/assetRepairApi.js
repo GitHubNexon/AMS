46d1aa06bf86d7d3b315768b31b731aa0cf3aa30
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-06-02 10:36:32 +0800

    added the logic api for assers repair

--- client/src/api/assetRepairApi.js
+++ client/src/api/assetRepairApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_BASE_URL } from "./config.js";
 
-const assetsRepairApi = {
+const assetRepairApi = {
   createAssetsRepairRecord: async (data) => {
     try {
       const response = await axios.post(
@@ -90,3 +90,5 @@ const assetsRepairApi = {
     }
   },
 };
+
+export default assetRepairApi;
---
+++ client/src/hooks/AssetsRepariLogic.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import assetRepairApi from "../api/assetRepairApi";
+
+const AssetsRepairLogic = (
+  initialPage = 1,
+  initialLimit = 10,
+  status = ""
+) => {
+  const [repairRecords, setRepairRecords] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalItems, setTotalItems] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  const fetchRepairRecords = async () => {
+    setLoading(true);
+    try {
+      const response = await assetRepairApi.getAllAssetsRepairRecord(
+        initialPage,
+        initialLimit,
+        searchQuery,
+        sortBy,
+        sortOrder,
+        status
+      );
+      setRepairRecords(response.repairRecords);
+      setTotalItems(response.totalItems);
+      setTotalPages(Math.ceil(response.totalItems / initialLimit));
+    } catch (error) {
+      console.error("Error fetching assets:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRepairRecords();
+  }, [initialPage, initialLimit, searchQuery, sortBy, sortOrder, status]);
+
+  return {
+    fetchRepairRecords,
+    setRepairRecords,
+    repairRecords,
+    totalItems,
+    totalPages,
+    loading,
+    setLoading,
+    setTotalItems,
+    setTotalPages,
+    searchQuery,
+    setSearchQuery,
+    sortBy,
+    setSortBy,
+    sortOrder,
+    setSortOrder,
+  };
+};
