6cdaa21a9156bb073650444d78241f464ac59bd8
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-06-04 09:29:49 +0800

    added the front end api for asset report

---
+++ client/src/Sub-pages/AssetsHistory.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useCallback } from "react";
+import assetReportApi from "../api/assetReportApi";
+import { numberToCurrencyString } from "../helper/helper";
+import showDialog from "../utils/showDialog";
+import { showToast } from "../utils/toastNotifications";
+import EmployeePicker from "../Components/EmployeePicker";
+import AssetsPicker from "../Components/AssetsPicker";
+
+
+const AssetsHistory = () => {
+  return <div>AssetsHistory</div>;
+};
+
+export default AssetsHistory;
---
+++ client/src/api/assetReportApi.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { API_BASE_URL } from "./config.js";
+
+const assetReportApi = {
+  getAssetsHistory: async (assetId, employeeId, inventoryId, filter) => {
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}/assets-reports/history`,
+        {
+          assetId,
+          employeeId,
+          inventoryId,
+          filter,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error creating assets history:", error.message);
+      throw error;
+    }
+  },
+};
+
+export default assetReportApi;
